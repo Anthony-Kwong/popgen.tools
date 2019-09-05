eb833849f9787c664a918c2ea5e1e5b759a9c34f
deponent-verb <deponentverb@hotmail.com>
2019-09-06 08:05:18 +0930

    update to test-hstat

--- tests/testthat/test-h_stats.R
+++ tests/testthat/test-h_stats.R
@@ -2,16 +2,23 @@ test_that("h_stats computed correctly",{
   set.seed(1989)
   #default, this generates 4 unique rows
   seq <-matrix(sample(0:1, size = 16, replace = TRUE), nc = 4)
-  #might turn this bit into a function later
-  copy2<-6
-  copy3<-4
-  copy4<-5
+
+  #we will duplicate the following rows
+  copy2<-2
+  copy3<-3
+  copy4<-4
   
-  for(i in 1:copy2){
-    seq<-rbind(seq,seq[2,])
-  }
+  N2<-3
+  N3<-5
+  N4<-7
   
-  seq<-rbind(seq,seq[2,])
-  seq<-rbind(seq,seq[4,])
-  seq<-rbind(seq,seq[3,])
+  seq<-row_replicate(seq,row=copy2,n=N2)
+  seq<-row_replicate(seq,row=copy3,n=N3)
+  seq<-row_replicate(seq,row=copy4,n=N4)
+  
+  count=c(1,1,1,1)+c(0,N2,N3,N4)
+  freq=count/nrow(seq)
+  
+  output<-h_stats(seq)
+  nrow(seq)
 })
