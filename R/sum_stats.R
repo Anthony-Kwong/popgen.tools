64238410b7e6201c64dec108641d5d619e186a32
deponent-verb <deponentverb@hotmail.com>
2019-10-06 12:15:15 +1030

    finished sum_stats function
    Will add tests later.

--- R/sum_stats.R
+++ R/sum_stats.R
@@ -7,6 +7,7 @@
 #' @importFrom tibble enframe as_tibble
 #' @importFrom purrr map2 pmap
 #' @importFrom dplyr bind_cols
+#' @import magrittr
 #' @return list of summary stats for the input genome matrix
 #' @export
 #' @examples sum_stats(win_list)
@@ -19,11 +20,11 @@ sum_stats<-function(sim,win_split){
   
   #Fay and Wu H, fwh(t_w,t_h)----
   H<-purrr::map2(basic_values$theta_w,basic_values$theta_h,fwh) %>% unlist()
-  H<-H %>% tibble::enframe(name=NULL,value="FW_H") 
+  #H<-H %>% tibble::enframe(name=NULL,value="FW_H") 
   
   #Tajima'D taj_D(t_t, t_w, var_taj)----
   D<-purrr::pmap(list(basic_values$theta_t,basic_values$theta_w,basic_values$var_taj),taj_D) %>% unlist()
-  D<-D %>% tibble::enframe(name=NULL,value="Taj_D") 
+  #D<-D %>% tibble::enframe(name=NULL,value="Taj_D") 
   
   # Haplotype stats (h1,h2,h12)----
   h_values<-lapply(win_list,h_stats) 
@@ -49,24 +50,44 @@ sum_stats<-function(sim,win_split){
   width<-floor(whole_win_width/win_split)
   mut_index<-ceiling(sweep_pos/width)
   
+  #updating position vector
+  position[mut_index]<-"mut"
   
-
-
+  if((mut_index-1)>0){
+    position[mut_index-1]="close"
+  }
+  
+  if((mut_index+1)<whole_win_width){
+    position[mut_index+1]="close"
+  }
+  
+  #position<-position %>% tibble::tibble()
+  #names(position)<-"position"
+  #position$position<- position$position %>% as.factor()
+  
+  #Various extra details about the simulation
+  snp<-sim$num_seg
+  snp<-rep(snp,win_split)
+  
+  sweep<-sim$sweep
+  sweep<-rep(sweep,win_split)
   
   #tying everything back together. ----
   #Tibble is great in giving neat names without the $. 
-  pi_est<-tibble::tibble(basic_values$theta_t)
-  names(pi_est)<-"pi"
-  df<-dplyr::bind_cols(D,H,h_df,pi_est)
+  pi_est<-basic_values$theta_t
+  df<-tibble::tibble(sweep,snp,position,D,H,pi_est)
+  df<-dplyr::bind_cols(df,h_df)
+  
+  #change sweep and position into factors.
+  df[,c("sweep","position")]<-lapply(df[,c("sweep","position")],as.factor)
 
   return(df)
 }
 
 #inserting for building purposes. Will remove. This bit causes trouble if left in. 
- data<-readRDS("~/work/MPhil/data/toy_set.rds")
- sim<-data[[1]]
- win_split=5
- #win_list<-sub_win(sim,n_win)
+ # data<-readRDS("~/work/MPhil/data/toy_set.rds")
+ # sim<-data[[1]]
+ # win_split=5
 
 ##### This version works hurray!
 
