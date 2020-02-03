3a1ca6b4832d203be8234597e3916e5e3b0deae1
deponent-verb <deponentverb@hotmail.com>
2020-02-03 11:10:27 +1030

    update on test-generate_df
    updated test to check for fun="norm" case.

--- R/generate_df.R
+++ R/generate_df.R
@@ -5,13 +5,15 @@
 #' @param sim_list list of simulation objects
 #' @param win_split number of subwindows desired to split each genome matrix per simulation
 #' @param snp: number of snps to include per simulation
+#' @param form: Option for dataframe to be in "tall" or "wide" form. 
+#' @param fun: function to apply over summary statistics (haplotype stats excluded). See sum_stats documentation for details. 
 #' @importFrom purrr pmap
 #' @importFrom dplyr bind_rows
 #' @return dataframe containing summary statistics of all the subwindows across all the simulations. 
 #' @export 
 #'
 #' @examples generate_df(sim_list,10)
-generate_df<-function(sim_list,win_split,snp){
+generate_df<-function(sim_list,win_split,snp,form="wide",fun="none"){
   num_sim<-length(sim_list)
   
   #extend arguments into vectors for pmap
@@ -19,8 +21,10 @@ generate_df<-function(sim_list,win_split,snp){
   id<-(1:num_sim)
   split<-rep(win_split,num_sim)
   snp<-rep(snp,num_sim)
+  form<-rep(form,num_sim)
+  fun<-rep(fun,num_sim)
   
-  df_list<-purrr::pmap(list(sim_list,split,id,snp),sum_stats)
+  df_list<-purrr::pmap(list(sim_list,split,id,snp,form,fun),sum_stats)
   df<-dplyr::bind_rows(df_list)
   
   #change sweep and position into factors.
--- tests/testthat/test-generate_df.R
+++ tests/testthat/test-generate_df.R
@@ -1,13 +1,13 @@
 test_that("generate_df works",{
   
-  #hard sweep simulation test
+  #hard sweep simulation test, checking fun="norm"
   
   #input parameters
-  mu=1e-8
+  mu=1.5e-8
   recomb_rate=1e-9
-  Ne=10000
-  genome_length=1e5
-  samplesize=20
+  Ne=1000
+  genome_length=1e6
+  samplesize=10
   s=0.01
   fix=1
   discoal_path="~/work/programs/discoal/discoal"
@@ -21,22 +21,23 @@ test_that("generate_df works",{
   }
   
   snp_include=50
-  nwins=5
+  nwins=3
   
-  df<-generate_df(l_sim,win_split = nwins,snp = snp_include)
+  df<-generate_df(l_sim,win_split = nwins,snp = snp_include,form="wide",fun="norm")
   
-  index=2
-  batch_ans<-df[index,] 
-  batch_ans<-as.numeric(batch_ans)
+  #check row 3 numbers
   
-  single_ans<-sum_stats(l_sim[[index]],win_split = nwins, ID=index, snp=snp_include)
-  single_ans$sweep<-as.factor(single_ans$sweep)
-  single_ans<-as.numeric(single_ans)
+  for(index in 1:nsim){
+    batch_ans<-df[index,] 
+    batch_ans<-as.numeric(batch_ans)
+    single_ans<-sum_stats(l_sim[[index]],win_split = nwins, ID=index, snp=snp_include,fun="norm")
+    single_ans$sweep<-as.factor(single_ans$sweep)
+    single_ans<-as.numeric(single_ans)
+    expect_equal(batch_ans,single_ans)
+  }
   
-  expect_equal(batch_ans,single_ans)
-  
-  #neutral test
-  
+## neutral test with default params for form and fun---
+
   #input parameters
   mu=1e-8
   recomb_rate=1e-9
@@ -54,16 +55,56 @@ test_that("generate_df works",{
   }
   
   snp_include=100
+  nwins=4
   
   df<-generate_df(l_sim,win_split = nwins,snp = snp_include)
   
-  index=4
-  batch_ans<-df[index,] 
-  batch_ans<-as.numeric(batch_ans)
+  for(index in 1:nsim){
+    batch_ans<-df[index,] 
+    batch_ans<-as.numeric(batch_ans)
+    single_ans<-sum_stats(l_sim[[index]],win_split = nwins, ID=index, snp=snp_include)
+    single_ans$sweep<-as.factor(single_ans$sweep)
+    single_ans<-as.numeric(single_ans)
+    expect_equal(batch_ans,single_ans)
+  }
   
-  single_ans<-sum_stats(l_sim[[index]],win_split = nwins, ID=index, snp=snp_include)
-  single_ans$sweep<-as.factor(single_ans$sweep)
-  single_ans<-as.numeric(single_ans)
+  ## hard sweep test with default params for form and fun
   
-  expect_equal(batch_ans,single_ans)
+  #hard sweep simulation test, checking fun="norm"
+  
+  #input parameters
+  mu=1.6e-8
+  recomb_rate=1e-9
+  Ne=1000
+  genome_length=1e6
+  samplesize=10
+  s=0.005
+  fix=2
+  discoal_path="~/work/programs/discoal/discoal"
+  sweep="hard"
+  nsim=10
+  
+  l_sim<-list()
+  
+  for(i in 1:nsim){
+    l_sim[[i]]<-discoal_sim(mu=mu,recomb_rate=recomb_rate,Ne=Ne,genome_length=genome_length,samplesize=samplesize,s=s,discoal_path=discoal_path,fix_generation=fix,sweep=sweep)
+  }
+  
+  snp_include=50
+  nwins=5
+  
+  df<-generate_df(l_sim,win_split = nwins,snp = snp_include,form="wide")
+  
+  #check row 3 numbers
+  
+  for(index in 1:nsim){
+    batch_ans<-df[index,] 
+    batch_ans<-as.numeric(batch_ans)
+    single_ans<-sum_stats(l_sim[[index]],win_split = nwins, ID=index, snp=snp_include)
+    single_ans$sweep<-as.factor(single_ans$sweep)
+    single_ans<-as.numeric(single_ans)
+    expect_equal(batch_ans,single_ans)
+  }
+  
 })
+
