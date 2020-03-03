ac65a40df04712dea51bbe6401888781b91b4b19
deponent-verb <deponentverb@hotmail.com>
2020-03-03 14:32:59 +1030

    Update test-generate_df.R

--- tests/testthat/test-generate_df.R
+++ tests/testthat/test-generate_df.R
@@ -36,7 +36,7 @@ test_that("generate_df works",{
     expect_equal(batch_ans,single_ans)
   }
   
-## neutral test with default params for form and fun---
+## neutral test with default params for form and fun, sweep_type=mut---
 
   #input parameters
   mu=1e-8
@@ -106,5 +106,41 @@ test_that("generate_df works",{
     expect_equal(batch_ans,single_ans)
   }
   
+})
+
+test_that("generate_df works",{
+  #These are tests for split_type=base
+  
+  ## neutral test
+  
+  #input parameters
+  mu=1e-8
+  recomb_rate=1e-9
+  Ne=10000
+  genome_length=1e5
+  samplesize=20
+  discoal_path="~/work/programs/discoal/discoal"
+  sweep="neutral"
+  nsim=5
+  
+  l_sim<-list()
+  
+  for(i in 1:nsim){
+    l_sim[[i]]<-discoal_sim(mu=mu,recomb_rate=recomb_rate,Ne=Ne,genome_length=genome_length,samplesize=samplesize,discoal_path=discoal_path,sweep=sweep)
+  }
+  
+  snp_include=150
+  nwins=10
+  
+  df<-generate_df(l_sim,nwins = nwins,split_type="base",snp = snp_include)
+  
+  for(index in 1:nsim){
+    batch_ans<-df[index,] 
+    batch_ans<-as.numeric(batch_ans)
+    single_ans<-sum_stats(l_sim[[index]],split_type="base",nwins = nwins, ID=index, snp=snp_include)
+    single_ans$sweep<-as.factor(single_ans$sweep)
+    single_ans<-as.numeric(single_ans)
+    expect_equal(batch_ans,single_ans)
+  }
 })
 
