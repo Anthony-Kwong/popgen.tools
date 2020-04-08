0ccb144a14c776c921e6e36a5e83ba6476191ed2
work-Anthony <shingkwong@192-168-1-115.tpgi.com.au>
2020-04-08 10:21:57 +0930

    Update LD_calc.R
    added Kelly's ZnS

--- R/LD_calc.R
+++ R/LD_calc.R
@@ -8,12 +8,13 @@
 #' For underlying theory see
 #' w_max: DOI: 10.1534/genetics.103.025387  
 #' D': Lewontin RC. The Interaction of Selection and Linkage. I. General Considerations; Heterotic Models. Genetics. 1964 Jan;49(1):49-67. PMID: 17248194; PMCID: PMC1210557.
+#' Zns: Kelly, J.K., 1997. A test of neutrality based on interlocus associations. Genetics, 146(3), pp.1197-1206. Vancouver	
 #'
 #' @param G : A NumericMatrix designating a binary genome matrix consisting of 1's and 0's. 
 #' Each column is a SNP. Each row is a sampled individual.  
 #' 
 #'
-#' @return A tibble of 3 elements (LD_avg,LD_max,w_max)
+#' @return A tibble of 4 elements (LD_avg,LD_max,w_max, Zns)
 #' @export
 #'
 #' @examples   seq <-matrix(sample(0:1, size =16 , replace = TRUE), nc = 4)
@@ -29,7 +30,7 @@ LD_calc = function(G){
   
   # compute standardised D ----
   D = data$`D'`
-  D_values = D[upper.tri(D)]
+  D_values = D_stan[upper.tri(D)]
   LD_avg = mean (D_values)
   LD_max = max(D_values)
   
@@ -40,9 +41,14 @@ LD_calc = function(G){
   w = sapply(x, LD_w, r=r)
   w_max= max(w)
   
+  #compute Zns ----
+  r_values = r[upper.tri(r)]
+  Zns = mean(r_values)
+  
   df = tibble::tibble("LD_avg"= LD_avg, 
                       "LD_max" = LD_max,
-                      "w_max" = w_max)
+                      "w_max" = w_max,
+                      "Zns" = Zns)
   #print("done")
   return(df)
 }
