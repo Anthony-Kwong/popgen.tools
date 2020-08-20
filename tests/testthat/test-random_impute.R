7a41d1c59cea9262c0395ea98305f1986a9cfb15
deponent-verb <deponentverb@hotmail.com>
2020-08-20 13:47:15 +0930

    Update test-random_impute.R

--- tests/testthat/test-random_impute.R
+++ tests/testthat/test-random_impute.R
@@ -1,3 +1,21 @@
+R_random_impute <- function (G){
+  output = G
+  sites = ncol(G)
+  nsam = nrow(G)
+  for(c in 1:sites){
+    col = G[,c]
+    na_pos = which(is.na(col))
+    num_na = length(na_pos)
+    col = purrr::discard(col,is.na)
+    for (r in 1:num_na){
+      imp = runif(1,min=0,max=length(col))
+      imp_index = round(imp)
+      output[na_pos[r],c] = col[imp_index]
+    }
+  }
+  return (output)
+}
+
 test_that("random_impute works",{
   #manual check
   set.seed(2)
@@ -7,9 +25,12 @@ test_that("random_impute works",{
   G[5,2] = NA
   set.seed(3)
   out = random_impute(G)
+  set.seed(3)
+  R_says = R_random_impute(G)
   
   G[1,1] = 0
   G[5,2] = 0
   G[3,4] = 1
   expect_equal(out, G)
+  expect_equal(out, R_says)
 })
